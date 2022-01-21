565721cb6e79c246fb74b233188d5d8dc7b97574
hmzsjd <github@hamzasajid.com>
2022-01-21 15:28:46 -0700

    projects endpoints built and working

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,27 @@
 // build your `/api/projects` router here
+const express = require("express");
+const Project = require("./model");
+
+const router = express.Router();
+
+router.post("/", (req, res, next) => {
+  const project = req.body;
+
+  Project.add(project)
+    .then((project) => {
+
+      res.status(201).json(project);
+
+    })
+    .catch(next);
+});
+
+router.get("/", (req, res, next) => {
+  Project.find()
+    .then((projects) => {
+      res.json(projects);
+    })
+    .catch(next);
+});
+
+module.exports = router;
